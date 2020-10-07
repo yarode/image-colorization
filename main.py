db9e877a9312f51d6bcd5c5b0abc5eb06263eef2
yarode <jaspervdvorst@gmail.com>
2020-10-07 22:39:02 +0200

    more complex implementation

--- main.py
+++ main.py
@@ -11,40 +11,73 @@ import random
 import tensorflow as tf
 
 # Get images
-image = img_to_array(load_img('./images/woman.jpg'))
-image = np.array(image, dtype=float)
-# Import map images into the lab colorspace
-X = rgb2lab(1.0/255*image)[:,:,0]
-Y = rgb2lab(1.0/255*image)[:,:,1:]
-Y = Y / 128
-X = X.reshape(1, 400, 400, 1)
-Y = Y.reshape(1, 400, 400, 2)
+X = []
+for filename in os.listdir('../Train/')
+    X.append(img_to_array(load_img('../Train/'+filename)))
+X = np.array(X, dtype=float)
+
+split = int(0.95*len(X))
+Xtrain = X[:split]
+Xtrain = 1.0/255*Xtrain
 
 # Building the nerual network
 model = Sequential()
-model.add(InputLayer(input_shape=(None, None, 1)))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+model.add(InputLayer(input_shape=(256, 256, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+model.add(UpSampling2D((2, 2)))
 
 model.compile(optimizer='rmsprop', loss='mse')
 
-model.fit(x=X, y=Y, batch_size=1, epochs=100)
-print(model.evaluate(X, Y, batch_size=1))
+datagen = ImageDataGenerator(
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=20,
+        horizontal_flip=True)
 
-output = model.predict(X)
+batch_size = 50
+def image_a_b_gen(batch_size):
+    for batch in datagen.flow(Xtrain, batch_size=batch_size):
+        lab_batch = rgb2lab(batch)
+        X_batch = lab_batch[:,:,:,0]
+        Y_batch = lab_batch[:,:,:,1:] / 128
+        yield (X_batch.reshape(X_batch.shape+(1,)), Y_batch)
+
+TensorBoard(log_dir='/output')
+model.fit_generator(image_a_b_gen(batch_size), steps_per_epoch=10000, epochs=1)
+# Test images
+Xtest = rgb2lab(1.0/255*X[split:])[:,:,:,0]
+Xtest = Xtest.reshape(Xtest.shape+(1,))
+Ytest = rgb2lab(1.0/255*X[split:])[:,:,:,1:]
+Ytest = Ytest / 128
+print model.evaluate(Xtest, Ytest, batch_size=batch_size)
+
+# Load black and white images
+color_me = []
+for filename in os.listdir('../Test/'):
+        color_me.append(img_to_array(load_img('../Test/'+filename)))
+color_me = np.array(color_me, dtype=float)
+color_me = rgb2lab(1.0/255*color_me)[:,:,:,0]
+color_me = color_me.reshape(color_me.shape+(1,))
+
+# Test model
+output = model.predict(color_me)
 output = output * 128
-canvas = np.zeros((400, 400, 3))
-canvas[:,:,0] = X[0][:,:,0]
-canvas[:,:,1:] = output[0]
-imsave("./images/img_result.png", lab2rgb(canvas))
-imsave("./images/img_gray_scale.png", rgb2gray(lab2rgb(canvas)))
+# Output colorizations
+for i in range(len(output)):
+        cur = np.zeros((256, 256, 3))
+        cur[:,:,0] = color_me[i][:,:,0]
+        cur[:,:,1:] = output[i]
+        imsave("result/img_"+str(i)+".png", lab2rgb(cur))
